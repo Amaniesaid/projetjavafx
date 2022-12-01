8438903f570c8c0bc4db210e2d8a7c94c42ef519
Amaniesaid <saidamanie2002@gmail.com>
2022-12-01 11:58:02 +0100

    ModifSecondaryView

--- src/main/java/com/tpinterface/SecondaryController.java
+++ src/main/java/com/tpinterface/SecondaryController.java
@@ -1,12 +1,37 @@
 package com.tpinterface;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        
     }
+
+    @FXML
+    private Button secondaryButton;
+
+    @FXML
+    private Button textcolor;
+
+    @FXML
+    private Label txtchange;
+
+    /**
+     * @param event
+     */
+    @FXML
+    void ChangeTextColor(ActionEvent event) {
+        textcolor.setStyle("-fx-text-fill: #FF0000");
+        txtchange.setText("Coucou !");
+    }
+
+
+    
 }
